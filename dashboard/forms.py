83693a10422bd0e746122354e9a57f315915c69d
rossyvee <rossyvee@gmail.com>
2023-04-22 19:08:52 +0300

    update the app with the registration template and the registering the URL in the main application

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.forms import widgets
 from . models import *
+from django.contrib.auth.forms import UserCreationForm
 
 class NotesForm(forms.ModelForm):
     class Meta:
@@ -25,5 +26,33 @@ class TodoForm(forms.ModelForm):
         fields=['title','is_finished']
 
 class ConversionForm(forms.Form):
-    CHOICES = [('length', 'Length'),('mass', 'Mass')]
-    measurement = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect)
+    CHOICES = [('length','Length'),('mass','Mass')]
+    measurement = forms.ChoiceField(choices = CHOICES, widget=forms.RadioSelect)
+
+class ConversionLengthForm(forms.Form):
+    CHOICES = [('yard','Yard'),('foot','Foot')]
+    input = forms.CharField(required = False,label = False,widget = forms.TextInput(attrs={'type':'number','placeholder':'Enter the Number'}))
+    measure1 = forms.CharField(
+        label='',widget=forms.Select(choices = CHOICES)
+    )
+
+    measure2 = forms.CharField(
+        label='',widget=forms.Select(choices = CHOICES)
+    )
+
+
+class ConversionMassForm(forms.Form):
+    CHOICES = [('pound','Pound'),('kilogram','Kilogram')]
+    input = forms.CharField(required=False,label=False,widget=forms.TextInput(attrs={'type':'number','placeholder':'Enter the Number'}))
+    measure1 = forms.CharField(
+        label='',widget=forms.Select(choices = CHOICES)
+    )
+
+    measure2 = forms.CharField(
+        label='',widget=forms.Select(choices = CHOICES)
+    )
+
+class UserRegistrationForm(UserCreationForm):
+    class Meta:
+        model =User
+        fields =['username','password1','password2']
