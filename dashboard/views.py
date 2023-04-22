68ed80a483817949cdccc5b5bb3272eb4414f2ef
rossyvee <rossyvee@gmail.com>
2023-04-22 20:46:29 +0300

    create the login and register pages and update the urls to the main app and also adding the redirect urls in the settings.py file

--- dashboard/views.py
+++ dashboard/views.py
@@ -310,8 +310,16 @@ def conversion(request):
     return render(request,"dashboard/conversion.html",context)
 
 def register(request):
-    form= UserRegistrationForm
+    if request.method == 'POST':
+     form= UserRegistrationForm(request.POST)
+     if form.is_valid():
+         form.save()
+         username = form.cleaned_data.get('username')
+         messages.success(request,f"Account created for {username}!!")
+         return redirect("login")
+    else:
+        form =UserRegistrationForm()
     context = {
         'form':form
-    }
+         }
     return render(request,"dashboard/register.html",context)
