97cdf20830182faaeae3eeb56f92728008fa9e41
rossyvee <rossyvee@gmail.com>
2023-04-23 16:43:09 +0300

    add the log in required to various functions of the project

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,12 +5,13 @@ from django.views import generic
 from youtubesearchpython import VideosSearch
 import requests
 import wikipedia
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
     return render(request,'dashboard/home.html')
 
-
+@login_required
 def notes(request):
     if request.method == "POST":
         form =NotesForm(request.POST)
@@ -25,13 +26,16 @@ def notes(request):
     context ={'notes':notes, 'form':form}
     return render(request,'dashboard/notes.html',context)
 
+@login_required
 def delete_note(request,pk=None):
     Notes.objects.get(id=pk).delete()
     return redirect('notes') 
 
+
 class NotesDetailView(generic.DetailView):
     model=Notes
 
+@login_required
 def homework(request):
     if request.method == "POST":
         form = HomeworkForm(request.POST)
@@ -66,6 +70,7 @@ def homework(request):
     context= {'homeworks':homework,'homeworks_done':homework_done,'form':form}
     return render(request,'dashboard/homework.html',context)
 
+@login_required
 def update_homework(request,pk=None):
     homework =Homework.objects.get(id=pk)
     if homework.is_finished == True:
@@ -74,7 +79,7 @@ def update_homework(request,pk=None):
         homework.is_finished = True
     homework.save()
     return redirect('homework')
-
+@login_required
 def delete_homework(request,pk=None):
     Homework.objects.get(id=pk).delete()
     return redirect("homework")
@@ -111,6 +116,7 @@ def youtube(request):
     context={'form':form}
     return render(request,'dashboard/youtube.html',context)
 
+@login_required
 def todo(request):
     if request.method =='POST':
         form = TodoForm(request.POST)
@@ -147,7 +153,7 @@ def todo(request):
 
 
 
-
+@login_required
 def update_todo(request,pk=None):
     todo =Todo.objects.get(id=pk)
     if todo.is_finished == True:
@@ -157,10 +163,10 @@ def update_todo(request,pk=None):
         todo.save()
         return redirect('todo')
     
+@login_required   
 def delete_todo(request,pk=None):
     Todo.objects.get(id=pk).delete()
     return redirect("todo")
-
 
 
 def books(request):
@@ -326,6 +332,7 @@ def register(request):
          }
     return render(request,"dashboard/register.html",context)
 
+@login_required
 def profile(request):
     homeworks= Homework.objects.filter(is_finished=False,user=request.user)
     todos = Todo.objects.filter(is_finished=False,user=request.user)
